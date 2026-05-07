80cc88a9c237a6b291dd9023005e412ec46942e1
Batraz2004 <batraz.tolasovv@gmail.com>
2026-05-07 17:06:40 +0300

    creat auth modul script

---
+++ app/Auth.java
@@ -0,0 +1,65 @@
+package app;
+
+import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import app.Enums.RoleEnum;
+import app.data.Models.User;
+import app.services.UserService;
+
+public class Auth {
+    private static final String FILE_PATH = "app/data/users.json";
+
+    private String role;
+    private Boolean isAuthorized;
+    private String name;
+    private String password;
+
+    public Boolean login() {
+        // ввод лоига и пароля
+        Scanner in = new Scanner(System.in);
+        System.out.println("ваше имя:");
+        String name = in.nextLine();
+        System.out.println("ваш пароль:");
+        String password = in.nextLine();
+
+        // поиск в файле и присвоение роли
+        UserService userService = new UserService();
+        List<User> users = userService.getAll();
+
+        User founderUser = users.stream()
+                .filter(u -> u.name == name && u.password == password)
+                .findFirst()
+                .orElse(null);
+
+        Boolean result = founderUser != null;
+
+        if (result) {
+
+            this.name = founderUser.name;
+            this.password = founderUser.password;
+            this.role = founderUser.role;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public Boolean isAuthorized() {
+        return this.isAuthorized;
+    }
+
+    @Override
+    public String toString() {
+        String result = String.format("Name: %s, Role: %d", this.name, this.role);
+        return result;
+    }
+}
